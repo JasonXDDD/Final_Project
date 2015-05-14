2235dc928f26f2081f79539f603f5ef54cf5fdd5
爆頭專家 <tom19960222@gmail.com>
2015-05-15 03:12:57 +0800

    Variable name change

--- src/MainScreen/MainTest.java
+++ src/MainScreen/MainTest.java
@@ -5,6 +5,6 @@ package MainScreen;
  */
 public class MainTest {
     public static void main(String[] args){
-        MainFrame test = new MainFrame();
+        MainFrame testFrame = new MainFrame();
     }
 }
