eda9f8f0969d8624215dab1f44c96820fac3810b
JasonXDDD <jason841022@yahoo.com.tw>
2015-05-15 06:09:03 +0800

    [Debug] Change JPanel to JSplitPane on AccountData
    Change JPanel to JSplitePane on AccountData
    To fix change size problem with 2 internal frame
    Delete the function of free location with 2 internal frame

--- src/MainScreen/MainFrame.java
+++ src/MainScreen/MainFrame.java
@@ -10,7 +10,7 @@ import java.awt.event.ComponentListener;
  */
 public class MainFrame extends JFrame implements ComponentListener{
     private JLayeredPane MF_lpLayer;
-    private JPanel MF_pnAccoutData;
+    private JSplitPane test;
 
 
     private ifAccountTool ifAT;
@@ -24,19 +24,20 @@ public class MainFrame extends JFrame implements ComponentListener{
         MF_lpLayer = this.getLayeredPane();
         MF_lpLayer.setLayout(null);
 
-        MF_pnAccoutData = new JPanel();
-        MF_pnAccoutData.setLayout(null);
-        MF_pnAccoutData.setBackground(Color.yellow);
-        MF_pnAccoutData.setSize(280,1080);
-        getContentPane().add(MF_pnAccoutData, BorderLayout.WEST);
+        test = new JSplitPane(JSplitPane.VERTICAL_SPLIT, true);
+        test.setDividerLocation(400);
+        test.setOneTouchExpandable(true);
+        test.setDividerSize(10);
+        test.setSize(280,1015);
+        getContentPane().add(test, BorderLayout.WEST);
 
         ifAT_iconHead = new ImageIcon(getClass().getResource("account_head.png"));
         ifAT = new ifAccountTool(ifAT_iconHead, "柯博文", 37, 100);
         ifAT.setBounds(100, 100, 280, 400);
-        MF_lpLayer.add(ifAT, new Integer(2500));
+        //MF_lpLayer.add(ifAT, new Integer(2500));
         ifAT.addComponentListener(this);
 
-
+        test.add(ifAT);
 
         ifBT_strBookName = new String[]{"Book1-XDD", "Book2-QAQ", "Book3-OwO",
                 "Book1-XDD", "Book1-XDD", "Book1-XDD", "Book1-XDD", "Book1-XDD",
@@ -45,10 +46,10 @@ public class MainFrame extends JFrame implements ComponentListener{
                 "Book1-XDD", "Book1-XDD", "Book1-XDD", "Book1-XDD", "Book1-XDD",};
         ifBT = new ifBookTool(3, ifBT_strBookName);
         ifBT.setBounds(500, 100, 280, 610);
-        MF_lpLayer.add(ifBT, new Integer(2500));
+        //MF_lpLayer.add(ifBT, new Integer(2500));
         ifBT.addComponentListener(this);
 
-
+        test.add(ifBT);
 
         setVisible(true);
         getContentPane().setBackground(Color.DARK_GRAY);
@@ -69,37 +70,38 @@ public class MainFrame extends JFrame implements ComponentListener{
 
     @Override
     public void componentMoved(ComponentEvent e) {
-        if(e.getSource() == ifAT){
-            if(ifAT.getX() <= 0){
-                MF_pnAccoutData.add(ifAT);
-                if(MF_pnAccoutData.getComponentCount() == 1)
-                    ifAT.setBounds(0, 0, 280, 1015);
-                else
-                    ifAT.setSize(280, 400);
-            }
-
-            else{
-                ifAT.setSize(280, 400);
-                if(MF_pnAccoutData.getComponent(1) == ifAT)
-                    MF_lpLayer.add(ifAT, 2500);
-            }
-        }
-
-        if(e.getSource() == ifBT){
-            if(ifBT.getX() <= 0){
-                MF_pnAccoutData.add(ifBT);
-                if(MF_pnAccoutData.getComponentCount() == 1)
-                    ifBT.setBounds(0, 0, 280, 1015);
-                else
-                    ifBT.setBounds(0, 615, 280, 610);
-            }
-
-            else{
-                ifBT.setSize(280, 610);
-                if(MF_pnAccoutData.getComponentCount() != 0)
-                    MF_lpLayer.add(ifBT, 2500);
-            }
-        }
+//        if(e.getSource() == ifAT){
+//            if(ifAT.getX() <= 0){
+//                test.add(ifAT);
+//                if(test.getComponentCount() == 1)
+//                    ifAT.setBounds(0, 0, 280, 1015);
+//                else
+//                    ifAT.setSize(280, 400);
+//
+//            }
+//
+//            else{
+//                if(test.getComponentCount() != 0)
+//                ifAT.setSize(280, 400);
+//
+//            }
+//        }
+//
+//        if(e.getSource() == ifBT){
+//            if(ifBT.getX() <= 0){
+//                test.add(ifBT);
+//                if(test.getComponentCount() == 1)
+//                    ifBT.setBounds(0, 0, 280, 1015);
+//                else
+//                    ifBT.setBounds(0, 615, 280, 610);
+//            }
+//
+//            else{
+//                ifBT.setSize(280, 610);
+//                if(test.getComponentCount() != 0)
+//                    MF_lpLayer.add(ifBT, 2500);
+//            }
+//        }
     }
 
     @Override
